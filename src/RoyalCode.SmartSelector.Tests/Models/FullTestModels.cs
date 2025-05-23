c0a946691664f9e6d98aad39fd940b9335cb7c7a
eglauko <eglauko@hotmail.com>
2025-05-23 08:56:38 -0300

    more tests

---
+++ src/RoyalCode.SmartSelector.Tests/Models/FullTestModels.cs
@@ -0,0 +1,156 @@
+﻿using System.Linq.Expressions;
+
+namespace RoyalCode.SmartSelector.Tests.Models;
+
+public class EntityWithSubSelectsAndCollections
+{
+    public int Id { get; set; }
+
+    public EntitySubTypeWithCollection SubValue { get; set; } = null!;
+
+    public ICollection<EntitySubType> SubTypes { get; set; } = null!;
+}
+
+public class EntitySubType
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    // a nullable entity property and a non-nullable dto property
+    public int? Money { get; set; }
+
+    public string OtherProperty { get; set; } = null!;
+}
+
+public class EntitySubTypeWithCollection
+{
+    public int Id { get; set; }
+
+    public ICollection<EntityItemWithSubType> CollectionOfSubItems { get; set; } = null!;
+}
+
+public class EntityItemWithSubType
+{
+    public int Id { get; set; }
+
+    public EntityWithCollectionOfItem PropertyWithItem { get; set; } = null!;
+
+    public EntityWithCollectionOfInt PropertyWithInt { get; set; } = null!;
+}
+
+public class EntityWithCollectionOfItem
+{
+    public int Id { get; set; }
+
+    public ICollection<EntityItem> Values { get; set; } = null!;
+}
+
+public class EntityItem
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
+
+public class EntityWithCollectionOfInt
+{
+    public int Id { get; set; }
+
+    public ICollection<int> Values { get; set; } = null!;
+}
+
+[AutoSelect<EntityWithSubSelectsAndCollections>]
+public partial class DtoWithSubSelectsAndCollections
+{
+    public int Id { get; set; }
+
+    public DtoSubTypeWithCollection SubValue { get; set; } = null!;
+
+    public IEnumerable<DtoSubType> SubTypes { get; set; } = null!;
+}
+
+public class DtoSubType
+{
+    public int Id { get; set; }
+
+    // a dto property with ? and a non-nullable entity property
+    public string? Name { get; set; }
+
+    public int Money { get; set; }
+}
+
+public class DtoSubTypeWithCollection
+{
+    public int Id { get; set; }
+
+    public IEnumerable<DtoItemWithSubType> CollectionOfSubItems { get; set; } = null!;
+}
+
+public class DtoItemWithSubType
+{
+    public int Id { get; set; }
+
+    public DtoWithCollectionOfItem PropertyWithItem { get; set; } = null!;
+
+    public DtoWithCollectionOfInt PropertyWithInt { get; set; } = null!;
+}
+
+public class DtoWithCollectionOfInt
+{
+    public IEnumerable<int> Values { get; set; } = null!;
+}
+
+public class DtoWithCollectionOfItem
+{
+    public int Id { get; set; }
+
+    public IEnumerable<DtoItem> Values { get; set; } = null!;
+}
+
+public class DtoItem
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
+
+
+public partial class DtoWithSubSelectsAndCollections
+{
+    private static Func<EntityWithSubSelectsAndCollections, DtoWithSubSelectsAndCollections> selectEntityWithSubSelectsAndCollectionsFunc;
+
+    public static Expression<Func<EntityWithSubSelectsAndCollections, DtoWithSubSelectsAndCollections>> SelectEntityWithSubSelectsAndCollectionsExpression { get; } = a => new DtoWithSubSelectsAndCollections
+    {
+        Id = a.Id,
+        SubValue = new DtoSubTypeWithCollection
+        {
+            Id = a.SubValue.Id,
+            CollectionOfSubItems = a.SubValue.CollectionOfSubItems.Select(b => new DtoItemWithSubType
+            {
+                Id = b.Id,
+                PropertyWithItem = new DtoWithCollectionOfItem
+                {
+                    Id = b.PropertyWithItem.Id,
+                    Values = b.PropertyWithItem.Values.Select(c => new DtoItem
+                    {
+                        Id = c.Id,
+                        Name = c.Name
+                    })
+                },
+                PropertyWithInt = new DtoWithCollectionOfInt
+                {
+                    Values = b.PropertyWithInt.Values
+                }
+            })
+        },
+        SubTypes = a.SubTypes.Select(b => new DtoSubType
+        {
+            Id = b.Id,
+            Name = b.Name,
+            Money = b.Money.HasValue ? b.Money.Value : default
+        })
+    };
+
+    public static DtoWithSubSelectsAndCollections From(EntityWithSubSelectsAndCollections entityWithSubSelectsAndCollections) => (selectEntityWithSubSelectsAndCollectionsFunc ??= SelectEntityWithSubSelectsAndCollectionsExpression.Compile())(entityWithSubSelectsAndCollections);
+}
